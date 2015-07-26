1df9db04ba7e09708c240d20406667949bd4e528
Jonathan White <mrwhite@Jonathans-MacBook-Pro.local>
2015-07-25 22:32:11 -0700

    refactor: new object pattern for injector

--- extension/dist/inject.bundle.js
+++ extension/dist/inject.bundle.js
@@ -52,29 +52,70 @@
 	    script.parentNode.removeChild(script);
 	};
 
-	var Inject = {};
+	// var Inject = {};
+	//
+	// Inject.init = init;
 
-	Inject.init = init;
+	// var Inject = {
+	//     init: function init() {
+	//         console.log('initializing: inject');
+	//         this._injectIframe();
+	//         chrome.extension.onMessage.addListener(this._messageHandler); // listens to background
+	//     },
+	//     _iframe: '',
+	//     test: 'yoo',
+	//     _injectIframe: function _injectIframe() {
+	//         console.log('action: injecting iframe');
+	//         var self = this;
+	//         self._iframe = document.createElement('iframe');
+	//         self._iframe.src = chrome.extension.getURL('frame.html');
+	//         self._iframe.style.cssText = 'position:fixed; top:0; right:0; display:block;' +
+	//                                'width:350px;height:100%;z-index:1000;';
+	//         self._iframe.id = 'keypoints';
+	//         this.test = 'doo';
+	//         console.log('Look', this);
+	//         document.body.appendChild(self._iframe);
+	//     },
+	//     _messageHandler: function(request, sender, sendResponse) {
+	//         var self = this;
+	//         console.log('Received a message', request, sender, sendResponse);
+	//         console.log('this:', this);
+	//         console.log('<<<<', Inject);
+	//         console.log('<<<<', Inject.test);
+	//     }
+	// };
 
-	function init() {
-	    console.log('initializing: inject');
-	    _injectIframe();
-	    chrome.extension.onMessage.addListener(_messageHandler); // listens to background
-	}
+	var Inject = (function() {
+	    var inject = {
+	        iframe: null,
+	        init: init
+	    };
 
-	function _injectIframe() {
-	    console.log('action: injecting iframe');
-	    var iframe = document.createElement('iframe');
-	    iframe.src = chrome.extension.getURL('frame.html');
-	    iframe.style.cssText = 'position:fixed; top:0; right:0; display:block;' +
-	                           'width:350px;height:100%;z-index:1000;';
-	    iframe.id = 'keypoints';
-	    document.body.appendChild(iframe);
-	}
+	    inject.init = init;
+	    return inject;
 
-	function _messageHandler(request, sender, sendResponse) {
-	    console.log('Received a message', request, sender, sendResponse);
-	}
+	    function init() {
+	        console.log('initializing: inject');
+	        _injectIframe();
+	        chrome.extension.onMessage.addListener(_messageHandler); // listens to background
+	    }
+
+	    function _injectIframe() {
+	        console.log('action: injecting iframe');
+	        this.iframe = document.createElement('iframe');
+	        this.iframe.src = chrome.extension.getURL('frame.html');
+	        this.iframe.style.cssText = 'position:fixed; top:0; right:0; display:block;' +
+	                               'width:350px;height:100%;z-index:1000;';
+	        this.iframe.id = 'keypoints';
+	        document.body.appendChild(iframe);
+	    }
+
+	    function _messageHandler(request, sender, sendResponse) {
+	        var self = this;
+	        console.log('Received a message', request, sender, sendResponse);
+	        console.log('this:', this.iframe);
+	    }
+	}());
 
 	window.addEventListener('load', Inject.init());
 
